72d7bad48b741be5c3c31d3c2789ad97842070d1
Sasha Laundy <sasha.laundy@gmail.com>
2013-02-20 17:05:15 -0500

    Add function for shrinking

--- src/project2.c
+++ src/project2.c
@@ -15,6 +15,7 @@
 #define BACKGROUND 0
 
 void grow(unsigned char *mask, unsigned char *grown, int rows, int cols);
+void shrink(unsigned char *mask, unsigned char *shrunk, int rows, int cols);
 
 int main(int argc, char *argv[]) {
 
@@ -25,19 +26,22 @@ int main(int argc, char *argv[]) {
   int gain;
   unsigned char *mask = NULL;
   unsigned char *grown = NULL;
+  unsigned char *shrunk = NULL;
   char *inputfile;
   char *maskfile;
-  char *processedfile;
+  char *growfile;
+  char *shrinkfile;
 
-  if(argc < 5) {
-    printf("Usage: ./project2 <input file> <mask file> <altered file> <gain>\n");
+  if(argc != 2) {
+    printf("Usage: ./project2 <input file>\n");
     exit(-1);
   }
 
   inputfile = argv[1];
-  maskfile = argv[2];
-  processedfile = argv[3];
-  gain = atoi(argv[4]);
+  maskfile = "masked.pgm";
+  growfile = "grown.pgm";
+  gain = 2;
+  shrinkfile = "shrunk.pgm";
 
   /* read in the image */
   image = readPPM(&rows, &cols, &colors, inputfile);
@@ -50,8 +54,9 @@ int main(int argc, char *argv[]) {
   imagesize = (long)rows * (long)cols;
   mask = (unsigned char*)malloc(sizeof(unsigned char) * imagesize);
   grown = (unsigned char*)malloc(sizeof(unsigned char) * imagesize);
+  shrunk = (unsigned char*)malloc(sizeof(unsigned char) * imagesize);
 
-  /* mess with the image here   */
+  /* mask the image */
   for(i=0; i<imagesize; i++) {
     if (image[i].r > (gain * image[i].g)) {
       mask[i] = BACKGROUND;
@@ -61,20 +66,20 @@ int main(int argc, char *argv[]) {
     }
   } 
 
-  /* make copies of mask to use in image implementation */
-  //memcpy(grown, mask, imagesize);
-
   /* manipulate the images */
   grow(mask, grown, rows, cols);
+  shrink(mask, shrunk, rows, cols);
 
   /* write out the files */
   writePGM(mask, rows, cols, intensities, maskfile);
-  writePGM(grown, rows, cols, intensities, processedfile);
+  writePGM(grown, rows, cols, intensities, growfile);
+  writePGM(shrunk, rows, cols, intensities, shrinkfile);
 
   /* free the image memory */
   free(image);
   free(mask);
   free(grown);
+  free(shrunk);
 
   return(0);
 }
@@ -86,7 +91,6 @@ void grow(unsigned char *mask, unsigned char *grown, int rows, int cols) {
   int x, y, j, k;
 
   imagesize = (long)rows * (long)cols;
-  printf("%d\n", cols);
 
   for (i=0; i < imagesize; ++i){
 
@@ -125,7 +129,50 @@ void grow(unsigned char *mask, unsigned char *grown, int rows, int cols) {
   
 }
 
+/* iterates through mask and outputs shrunk */
+void shrink(unsigned char *mask, unsigned char *shrunk, int rows, int cols) {
 
+  long imagesize, i;
+  int x, y, j, k;
+
+  imagesize = (long)rows * (long)cols;
+
+  for (i=0; i < imagesize; ++i){
+
+    /* don't process the 1 pixel layer around the whole image */
+    if ((i < (cols)) || i > (imagesize - cols) || (i % cols) == 0 || (i % cols) == (cols-1)) {
+      shrunk[i] = mask[i];
+    } 
+    /* ignore if foreground */
+    else if (mask[i] == FOREGROUND) {
+      shrunk[i] = mask[i];
+    }
+    /* if background, apply shrinking algorithm */
+    else if (mask[i] == BACKGROUND) {
+      x = i % cols;
+      y = i / cols;
+
+      /* look at 8 nearest neighbors. If foreground, make i foreground too */
+      for (j = -1; j <= 1; ++j) {
+        for(k = -1; k <= 1; ++k) {
+          /* grab pixel at ((x+j), (y+k)) */
+          if (i<2000) printf("i %ld, (%d, %d), j %d\n", i, j, k, ((cols*(y+k)) + (x+j)) );
+          if (mask[ (cols*(y+k)) + (x+j) ] == FOREGROUND) {
+            shrunk[i] = FOREGROUND;
+          }
+          else {
+            shrunk[i] = 128;
+          }
+        }
+      }
+    }  
+    
+    else {
+      shrunk[i] = mask[i];
+    }
+  }
+  
+}
 
 
 
