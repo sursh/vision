7330f5f373473bccf8b8a14386451fce9f28caa5
Sasha Laundy <sasha.laundy@gmail.com>
2013-02-21 12:27:09 -0500

    Refactoring filenames and whitespace

--- src/project2.c
+++ src/project2.c
@@ -10,7 +10,7 @@
 #include "vision.h"
 
 #define USECPP 0
-#define intensities 255
+#define INTENSITIES 255
 #define FOREGROUND 255
 #define BACKGROUND 0
 
@@ -30,39 +30,31 @@ int main(int argc, char *argv[]) {
   unsigned char *shrunk = NULL;
   unsigned char *median = NULL;
   char *inputfile;
-  char *maskfile;
-  char *growfile;
-  char *shrinkfile;
-  char *medianfile;
 
-  if(argc != 2) {
-    printf("Usage: ./project2 <input file>\n");
+  if (argc != 2) {
+    printf("Usage: %s <input file>\n", argv[0]);
     exit(-1);
   }
 
   inputfile = argv[1];
-  maskfile = "masked.pgm";
-  growfile = "grown.pgm";
   gain = 2;
-  shrinkfile = "shrunk.pgm";
-  medianfile = "median.pgm";
 
   /* read in the image */
   image = readPPM(&rows, &cols, &colors, inputfile);
-  if(!image) {
+  if (!image) {
     fprintf(stderr, "Unable to read %s\n", inputfile);
     exit(-1);
   }
 
   /* calculate the image size */
   imagesize = (long)rows * (long)cols;
-  mask = (unsigned char*)malloc(sizeof(unsigned char) * imagesize);
-  grown = (unsigned char*)malloc(sizeof(unsigned char) * imagesize);
-  shrunk = (unsigned char*)malloc(sizeof(unsigned char) * imagesize);
-  median = (unsigned char*)malloc(sizeof(unsigned char) * imagesize);
+  mask = (unsigned char*) malloc(sizeof(unsigned char) * imagesize);
+  grown = (unsigned char*) malloc(sizeof(unsigned char) * imagesize);
+  shrunk = (unsigned char*) malloc(sizeof(unsigned char) * imagesize);
+  median = (unsigned char*) malloc(sizeof(unsigned char) * imagesize);
 
   /* mask the image */
-  for(i=0; i<imagesize; i++) {
+  for (i=0; i<imagesize; i++) {
     if (image[i].r > (gain * image[i].g)) {
       mask[i] = BACKGROUND;
     }
@@ -77,10 +69,10 @@ int main(int argc, char *argv[]) {
   medianify(mask, median, rows, cols);
 
   /* write out the files */
-  writePGM(mask, rows, cols, intensities, maskfile);
-  writePGM(grown, rows, cols, intensities, growfile);
-  writePGM(shrunk, rows, cols, intensities, shrinkfile);
-  writePGM(median, rows, cols, intensities, medianfile);
+  writePGM(mask, rows, cols, INTENSITIES, "masked.pgm");
+  writePGM(grown, rows, cols, INTENSITIES, "grown.pgm");
+  writePGM(shrunk, rows, cols, INTENSITIES, "shrunk.pgm");
+  writePGM(median, rows, cols, INTENSITIES, "median.pgm");
 
   /* free the image memory */
   free(image);
@@ -116,7 +108,7 @@ void medianify(unsigned char *mask, unsigned char *median, int rows, int cols) {
 
       /* look at 8 nearest neighbors. Average wins, i is tiebreaker */
       for (j = -1; j <= 1; ++j) {
-        for(k = -1; k <= 1; ++k) {
+        for (k = -1; k <= 1; ++k) {
 
           neighbor = ( (x+j) + (cols*(y+k)) );
 
@@ -124,14 +116,14 @@ void medianify(unsigned char *mask, unsigned char *median, int rows, int cols) {
             ++total;
           }
           
-          if (total > 4) {
-            median[i] = FOREGROUND;
-          }
-          else {
-            median[i] = BACKGROUND;
-          }
-
         }
+      }
+
+      if (total > 4) {
+        median[i] = FOREGROUND;
+      }
+      else {
+        median[i] = BACKGROUND;
       }
     }  
   }
@@ -161,8 +153,8 @@ void shrink(unsigned char *mask, unsigned char *shrunk, int rows, int cols) {
       y = i / cols;
 
       /* look at 8 nearest neighbors. If any are background, turn pixel off */
-      for (j = -1; j <= 1; ++j) {
-        for(k = -1; k <= 1; ++k) {
+      for (j = -1; (j <= 1) && (shrunk[i] != FOREGROUND); ++j) {
+        for (k = -1; k <= 1; ++k) {
 
           neighbor = ( (x+j) + (cols*(y+k)) );
 
@@ -175,11 +167,11 @@ void shrink(unsigned char *mask, unsigned char *shrunk, int rows, int cols) {
           }
           
         }
-        if (shrunk[i] == BACKGROUND) break; /* out of second loop */
       }
     }  
     
     else {
+      printf("Error: Hit a value that's not 0 or 255!\n");
       shrunk[i] = mask[i];
     }
   }
@@ -210,7 +202,7 @@ void grow(unsigned char *mask, unsigned char *grown, int rows, int cols) {
 
       /* look at 8 nearest neighbors. If any are foreground, turn pixel on */
       for (j = -1; j <= 1; ++j) {
-        for(k = -1; k <= 1; ++k) {
+        for (k = -1; k <= 1; ++k) {
 
           neighbor = ( (x+j) + (cols*(y+k)) );
 
@@ -225,6 +217,7 @@ void grow(unsigned char *mask, unsigned char *grown, int rows, int cols) {
     }  
     
     else {
+      printf("Error: Hit a value that's not 0 or 255!\n");
       grown[i] = mask[i];
     }
   }
